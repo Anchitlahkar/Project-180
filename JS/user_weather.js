b19cef57f8d7a9368fafe1619e7ded9bb81dcb01
Anchit Lahkar <anchitlahkar0202@gmail.com>
2022-03-11 21:45:41 +0530

    11.03.2022

--- JS/user_weather.js
+++ JS/user_weather.js
@@ -26,7 +26,7 @@ async function getWeather() {
                     <a-entity gps-entity-place="latitude: ${coordinates.location_lat}; longitude: ${coordinates.location_lng}">
                         <a-entity>
                             <a-text height="50" value="Weather: ${weather} at ${name}"></a-text>
-                            <a-text height="45" value="Temperature: ${celsius}°C"></a-text>
+                            <a-text height="60" value="Temperature: ${celsius}°C"></a-text>
                             <a-text height="40" value="Temperature: ${celsius}°C"></a-text>
                         </a-entity>
                     </a-entity>
